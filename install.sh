38ff66fc2e5fde3006f4108223850bf00d9cbda0
Toby Kurien <toby@tobykurien.com>
2020-07-05 17:36:08 +0200

    add onboard default settings

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing Raspberry Pi Tablet OS..."
 sudo apt update
 sudo apt full-upgrade
-sudo apt install -y onboard git libgles2-mesa libgles2-mesa-dev xorg-dev bluetooth bluez blueman pulseaudio libqt4-dev libx11-6 libxtst-dev libgrail6
+sudo apt install -y onboard git libgles2-mesa libgles2-mesa-dev xorg-dev bluetooth bluez blueman pulseaudio libqt4-dev libx11-6 libxtst-dev libgrail6 dconf-cli
 
 rm -rf rpi_tablet_os
 git clone https://github.com/tobykurien/rpi_tablet_os.git rpi_tablet_os
@@ -11,6 +11,8 @@ sudo cp -r * /
 
 sudo mkdir /usr/share/touchegg
 sudo ln -s ~/.config/touchegg/touchegg.conf /usr/share/touchegg/touchegg.conf
+
+dconf load /org/onboard/ < ~/onboard.dconf
 
 echo "Installation complete, rebooting..."
 sleep 10s
