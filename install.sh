cac7b6302226909daa37ee09c5d5c61b6f365bf3
Toby Kurien <toby@tobykurien.com>
2020-07-05 12:37:36 +0200

    wip

--- install.sh
+++ install.sh
@@ -2,5 +2,12 @@
 echo "Installing Raspberry Pi Tablet OS..."
 sudo apt update
 sudo apt full-upgrade
-sudo apt install onboard
-echo "More to come..."
+sudo apt install onboard git libgles2-mesa libgles2-mesa-dev xorg-dev bluetooth bluez blueman pulseaudio
+
+git clone https://github.com/tobykurien/rpi_tablet_os.git rpi_tablet_os
+cd rpi_tablet_os/fs
+sudo cp -r * /
+
+echo "Installation complete, rebooting..."
+sleep 10s
+sudo reboot
