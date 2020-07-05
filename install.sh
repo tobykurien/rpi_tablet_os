3d323df93b3e5cf7abd7133ab1ce67d66054492d
Toby Kurien <toby@tobykurien.com>
2020-07-05 13:05:51 +0200

    toucheg binaries

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing Raspberry Pi Tablet OS..."
 sudo apt update
 sudo apt full-upgrade
-sudo apt install -y onboard git libgles2-mesa libgles2-mesa-dev xorg-dev bluetooth bluez blueman pulseaudio
+sudo apt install -y onboard git libgles2-mesa libgles2-mesa-dev xorg-dev bluetooth bluez blueman pulseaudio libqt4-dev libx11-6 libxtst-dev libgrail6
 
 git clone https://github.com/tobykurien/rpi_tablet_os.git rpi_tablet_os
 cd rpi_tablet_os/fs
